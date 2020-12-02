f352fd9b5784735fcfc5f205ee5efe38aa696e5f
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:41:50 +0300

    Create 10.11.2020-3.java

---
+++ 10.11.2020-3.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class sdf {
+    int[] numbers = new int[3];
+try{
+        numbers[6]=45;
+        numbers[6]=Integer.parseInt("gfd");
+    }
+catch(ArrayIndexOutOfBoundsException ex){
+
+        System.out.println("Выход за пределы массива");
+    }
+catch(NumberFormatException ex){
+
+        System.out.println("Ошибка преобразования из строки в число");
+    }
+
+}
